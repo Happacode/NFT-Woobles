06d287423bec023ae115806f2796c8507e50e264
Happacode <happacode@protonmail.com>
2022-02-15 20:02:20 -0800

    Created first 10 unique art

--- hashlips_art_engine-1.1.2_patch_v5/src/config.js
+++ hashlips_art_engine-1.1.2_patch_v5/src/config.js
@@ -24,15 +24,13 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 20,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Backgrounds" },
+      { name: "Fur" },
+      { name: "Mouth" },
+      { name: "Eyes" },
+      { name: "Clothing" },
     ],
   },
 ];
